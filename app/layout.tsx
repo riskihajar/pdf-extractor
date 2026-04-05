f21bca4973ee00c097a5124d55d056270016b346
riskihajar <riskihajar@gmail.com>
2026-04-05 21:08:22 +0800

    feat(ui): add initial pipeline dashboard

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,13 @@
-import { Geist, Geist_Mono } from "next/font/google"
+import { Geist_Mono, Space_Grotesk } from "next/font/google"
 
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
-import { cn } from "@/lib/utils";
+import { cn } from "@/lib/utils"
 
-const geist = Geist({subsets:['latin'],variable:'--font-sans'})
+const fontSans = Space_Grotesk({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
 
 const fontMono = Geist_Mono({
   subsets: ["latin"],
@@ -20,7 +23,12 @@ export default function RootLayout({
     <html
       lang="en"
       suppressHydrationWarning
-      className={cn("antialiased", fontMono.variable, "font-sans", geist.variable)}
+      className={cn(
+        "antialiased",
+        fontMono.variable,
+        "font-sans",
+        fontSans.variable
+      )}
     >
       <body>
         <ThemeProvider>{children}</ThemeProvider>
